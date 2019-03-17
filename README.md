84a96166465fa140cbc8f9ce77ac82b593ecdbe8
Yansy <yansiyu.old.dog@hotmail.com>
2019-03-18 02:15:47 +0800

    snowflake basic

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # snowflake
 Generate distributed-id implementation Twitter id's Snowflake with Python 
+
+# todo
+thread Compete
+
+# check
+not sure right for now.
---
+++ snowflake.py
@@ -0,0 +1,75 @@
+"""
+Generate distributed-id implementation Twitter id's Snowflake with Python
+"""
+import functools
+import threading
+import time
+
+
+def synchronized(wrapped):
+    lock = threading.Lock()
+
+    @functools.wraps(wrapped)
+    def _wrap(*args, **kwargs):
+        print("Calling '%s' with Lock %s" % (wrapped.__name__, id(lock)))
+        with lock:
+            return wrapped(*args, **kwargs)
+
+    return _wrap
+
+
+class IdWorker:
+
+    def __init__(self, worker_id, datacenter_id, sequence):
+        self.worker_id = worker_id
+        self.datacenter_id = datacenter_id
+        self.sequence = sequence
+
+        self.epoch = 1288834974657
+        self.worker_id_bits = 5
+        self.max_worker_id_bits = -1 ^ (-1 << self.worker_id_bits)
+        self.datacenter_id_bits = 5
+        self.max_datacenter_id_bits = -1 ^ (-1 << self.datacenter_id_bits)
+        self.sequence_bits = 12
+        self.sequence_mask = -1 ^ (-1 << self.sequence_bits)
+        self.worker_id_shift = self.sequence_bits
+        self.datacenter_id_shift = self.worker_id_bits + self.sequence_bits
+        self.timestamp_left_shift = self.datacenter_id_bits + self.worker_id_bits + self.sequence_bits
+        self.last_timestamp = -1
+
+    def get_id(self):
+        return self.next_id()
+
+    @synchronized
+    def next_id(self):
+        timestamp = self.time_gen()
+        if timestamp < self.last_timestamp:
+            return 'error'
+        if self.last_timestamp == timestamp:
+            self.sequence = (self.sequence + 1) & self.sequence_mask
+            if self.sequence == 0:
+                timestamp = self.till_next_milliseconds(self.last_timestamp)
+        else:
+            self.sequence = 0
+        self.last_timestamp = timestamp
+        next_id = ((timestamp - self.epoch) << self.timestamp_left_shift) | (
+                self.datacenter_id << self.datacenter_id_shift) | (
+                          self.worker_id << self.worker_id_shift) | self.sequence
+        return next_id
+
+    def till_next_milliseconds(self, last_timestamp):
+        timestamp = self.time_gen()
+        while timestamp <= last_timestamp:
+            timestamp = self.time_gen()
+        return timestamp
+
+    @staticmethod
+    def time_gen():
+        timestamp = int(round(time.time() * 1000))
+        return timestamp
+
+
+if __name__ == '__main__':
+    worker = IdWorker(1, 1, 0)
+    print(worker.get_id())
+    # todo my_thread
